9a216fd5fa06f4b9eded03325c19e9f90a283447
Alexandre Costa <alexandre.gcosta@gmail.com>
2019-08-10 20:44:21 -0300

    #4: A tela de login foi convertida em componente

---
+++ frontend/src/pages/Login.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Login.css';
+
+import logo from '../assets/logo.svg';
+
+export default function Login() {
+    return (
+        <img src={logo} alt="Tindev" />
+    );
+}
