e8afbdc13f13afb65bfea299a726594c6e0261a4
Alexandre Costa <alexandre.gcosta@gmail.com>
2019-08-10 22:59:02 -0300

    #4: adicionado funcionalidade dislike e remove usuario da lista

--- frontend/src/pages/Main.js
+++ frontend/src/pages/Main.js
@@ -23,6 +23,18 @@ export default function Main ({ match }) {
 
         loadUsers();
     }, [match.params.id])
+    
+    async function handleDislike(id) {
+        await api.post(`/devs/${id}/dislikes`, null, {
+            headers: { user: match.params.id },
+        });
+
+        setUsers(users.filter(user => user._id !== id))
+    }
+
+    async function handleLike(id) {
+        console.log('like', id);
+    }
 
     return (
         <div className="main-container">
@@ -36,10 +48,10 @@ export default function Main ({ match }) {
                             <p>{user.bio}</p>
                         </footer>
                         <div className="buttons">
-                            <button type="button">
+                            <button type="button" onClick={() => handleDislike(user._id)}>
                                 <img src={dislike} alt="Dislike"/>
                             </button>
-                            <button type="button">
+                            <button type="button" onClick={() => handleLike(user._id)}>
                                 <img src={like} alt="Like"/>
                             </button>
                         </div>
