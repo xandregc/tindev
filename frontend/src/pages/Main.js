9eab16f907070a8eabf62bc6dd6955b580fb9615
Alexandre Costa <alexandre.gcosta@gmail.com>
2019-08-10 22:45:08 -0300

    #4: fazendo get para a api e armazenando o resultado em um estado

--- frontend/src/pages/Main.js
+++ frontend/src/pages/Main.js
@@ -1,11 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Main.css'
+
+import api from '../services/api';
 
 import logo from '../assets/logo.svg';
 import dislike from '../assets/dislike.svg';
 import like from '../assets/like.svg';
 
 export default function Main ({ match }) {
+    const [users, setUsers] = useState([]);
+
+    useEffect(() => {
+        async function loadUsers() {
+            const response = await api.get('/devs', {
+                headers: { 
+                   user: match.params.id
+                }
+            })
+
+            setUsers(response.data);
+        }
+
+        loadUsers();
+    }, [match.params.id])
+
     return (
         <div className="main-container">
             <img src={logo} alt="Tindev" />
